58ed12b39355170b77282b16641eb741fd7db0db
Ahmad Arsy <arsyahmad14@gmail.com>
2023-10-26 15:39:40 +0700

    Added update and delete support

--- internal/controller/todo-controller.go
+++ internal/controller/todo-controller.go
@@ -25,3 +25,35 @@ func CreateTodo(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, todoResult)
 }
+
+func UpdateTodo(context *gin.Context) {
+	var todo domain.Todo
+	id := context.Param("id")
+
+	if err := context.ShouldBindJSON(&todo); err != nil {
+		context.JSON(http.StatusUnprocessableEntity, "invalid json body")
+		return
+	}
+
+	todoResult, err := service.TodoService.UpdateTodo(&todo, id)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	context.JSON(http.StatusOK, todoResult)
+}
+
+func DeleteTodo(context *gin.Context) {
+	id := context.Param("id")
+
+	todoResult, err := service.TodoService.DeleteTodo(id)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	context.JSON(http.StatusOK, todoResult)
+}
